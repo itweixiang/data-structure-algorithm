1d950b82f990214c740f71f5452cfe7f252199c4
林蔚湘 <linwx@yealink.com>
2021-03-24 17:01:07 +0800

    中缀转后缀，后缀计算

--- data-structure/src/com/itweixiang/study/stack/SuffixExpression.java
+++ data-structure/src/com/itweixiang/study/stack/SuffixExpression.java
@@ -1,49 +1,121 @@
 package com.itweixiang.study.stack;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 import java.util.regex.Pattern;
 
+/**
+ * 判断数字问题，正则匹配
+ * 多位数拼接问题，先逐项进行拼接再进行计算，揉在一起不好处理
+ * 一些规则的，之前不了解
+ */
 public class SuffixExpression {
-    private Stack<String> stack;
+    Stack<String> s1 = new Stack<>();
+    Stack<String> s2 = new Stack<>();
 
-    public SuffixExpression(String infix) {
-        for (int i = 0; i < infix.length(); i++) {
-            char ch = infix.charAt(i);
-            if (isNum(ch)) {
-                stack.push(String.valueOf(ch));
-            } else {
-                String peek = stack.peek();
-                if (isNum(peek)) {
-                    stack.push(String.valueOf(ch));
-                } else if (priority()) {
-
-                } else if (priority(peek.charAt(0)) > priority(ch)) {
-                    String pop = stack.pop();
-                }
-            }
-        }
+    public static void main(String[] args) {
+        SuffixExpression suffixExpression = new SuffixExpression("1+((2+3)*4)-5");
+        System.out.println(suffixExpression.result());
     }
 
     public Integer result() {
-        return 0;
+        for (String s : s2) {
+            if (isNum(s)) {
+                s1.push(s);
+            } else {
+                String b = s1.pop();
+                String a = s1.pop();
+                s1.push(String.valueOf(cal(Integer.valueOf(a), Integer.valueOf(b), s)));
+            }
+        }
+        return Integer.valueOf(s1.pop());
     }
 
-    public Boolean isNum(char ch) {
-        return isNum(String.valueOf(ch));
+    public Integer cal(Integer a, Integer b, String oper) {
+        if ("*".equals(oper)) {
+            return a * b;
+        }
+        if ("/".equals(oper)) {
+            return a / b;
+        }
+        if ("+".equals(oper)) {
+            return a + b;
+        }
+        if ("-".equals(oper)) {
+            return a - b;
+        }
+        return -1;
     }
 
-    public Boolean isNum(String ch) {
-        return Pattern.matches("[0-9]", ch);
-    }
+    public SuffixExpression(String infix) {
+        List<String> split = split(infix);
 
-    public Integer priority(char ch) {
-        if (ch == '*' || ch == '/') {
-            return 1;
-        } else if (ch == '(' || ch == ')') {
-            return 2;
-        } else if (ch == '+' || ch == '-') {
-            return 0;
+        for (String s : split) {
+            if (isNum(s)) {
+                s2.push(s);
+            } else {
+                while (true) {
+                    if (s1.isEmpty() || "(".equals(s)
+                            || "(".equals(s1.peek()) || priority(s) > priority(s1.peek())) {
+                        s1.push(s);
+                        break;
+                    } else if (")".equals(s)) {
+                        while (true) {
+                            String pop = s1.pop();
+                            if ("(".equals(pop)) {
+                                break;
+                            } else {
+                                s2.push(pop);
+                            }
+                        }
+                        break;
+                    } else {
+                        s2.push(s1.pop());
+                    }
+                }
+            }
+        }
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
         }
     }
 
+    private List<String> split(String infix) {
+        List<String> list = new ArrayList<>();
+        String number = "";
+        for (int i = 0; i < infix.length(); i++) {
+            char ch = infix.charAt(i);
+            if (isNum(ch)) {
+                number += ch;
+            } else {
+                if (!"".equals(number)) {
+                    list.add(number);
+                    number = "";
+                }
+                list.add(String.valueOf(ch));
+            }
+        }
+        if (!"".equals(number)) {
+            list.add(number);
+        }
+        return list;
+    }
+
+    private Integer priority(String oper) {
+        if ("*".equals(oper) || "/".equals(oper)) {
+            return 1;
+        } else if ("+".equals(oper) || "-".equals(oper)) {
+            return 0;
+        }
+        return -1;
+    }
+
+    private Boolean isNum(char ch) {
+        return isNum(String.valueOf(ch));
+    }
+
+    private Boolean isNum(String str) {
+        return Pattern.matches("[0-9]+", str);
+    }
 }
