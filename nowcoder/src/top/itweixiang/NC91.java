125049d4260e12143c611ac6ca85bd8f8d096f26
lwx <itweixiang@126.com>
2021-07-28 20:06:31 +0800

    最长递增子序列 参考题解

--- nowcoder/src/top/itweixiang/NC91.java
+++ nowcoder/src/top/itweixiang/NC91.java
@@ -10,55 +10,103 @@ import java.util.*;
  */
 public class NC91 {
     public static void main(String[] args) {
-        int[] arr = {7, 123113, 7, -1, 8};
+        int[] arr = {1, 3, 8, 6, 5, 2};
         NC91 nc91 = new NC91();
         int[] lis = nc91.LIS(arr);
     }
-    /*
-    第一步——求最长递增子序列长度
-    第二步——求字典序靠前的子序列
 
-    贪心+二分的解法，举例说明基本思路，假设数组arr为[2, 3, 1, 2, 3]，
-    vec数组里面存放递增子序列，maxLen数组里存放以元素i结尾的最大递增子序列长度，那么遍历数组arr并执行如下更新规则:
-
-    初始情况下，vec为[2]，maxLen[1]
-    接下来遇到3，由于vec最后一个元素小于3，直接更新，vec为[2,3]，maxLen[1,2]
-    接下来遇到1，由于vec最后的元素大于1, 我们在vec中查找大于等于1的第一个元素的下标，并用1替换之，此时vec为[1,3], maxLen[1,2,1]
-    接下来遇到2，由于vec最后的元素大于2，我们在vec中查找大于等于2的第一个元素的下标，并用2替换之，此时vec为[1,2], maxLen[1,2,1,2]
-    接下来遇到3，由于vec最后一个元素小于3，直接更新，vec为[1,2,3]，maxLen为[1,2,1,2,3]
-    此时vec的大小就是整个序列中最长递增子序列的长度（但是vec不一定是本题的最终解）
-
-    对于第二步，假设我们原始数组是arr1，得到的maxLen为[1,2,3,1,3]，最终输出结果为res（字典序最小的最长递增子序列），
-    res的最后一个元素在arr1中位置无庸置疑是maxLen[i]==3对应的下标，那么到底是arr1[2]还是arr1[4]呢？如果是arr1[2]，
-    那么arr1[2]<arr1[4]，则maxLen[4]==4，与已知条件相悖。因此我们应该取arr1[4]放在res的最后一个位置。
-    vector<int> LIS(vector<int>& arr) {
-        // write code here
-        // 第一步：利用贪心+二分求最长递增子序列长度
-        vector<int> res;
-        vector<int> maxLen;
-        if (arr.size() < 1) return res;
-        res.emplace_back(arr[0]);  // 注：emplace_back(val)作用同push_back，效率更高
-        maxLen.emplace_back(1);
-        for (int i = 1; i < arr.size(); ++i) {
-            if (arr[i] > res.back()) {
-                res.emplace_back(arr[i]);
-                maxLen.emplace_back(res.size());
+    /**
+     * retrun the longest increasing subsequence
+     *
+     * @param arr int整型一维数组 the array
+     * @return int整型一维数组
+     */
+    public int[] LIS(int[] arr) {
+        //每一项子序列的长度
+        int[] nums = new int[arr.length];
+        int[] temp = new int[arr.length];
+        nums[0] = 1;
+        int tempIndex = 0;
+        temp[tempIndex] = arr[0];
+        for (int i = 1; i < arr.length; ++i) {
+            int left = 0, right = tempIndex;
+            if (arr[i] > temp[tempIndex]) {
+                ++tempIndex;
+                nums[i] = tempIndex + 1;
+                temp[tempIndex] = arr[i];
             } else {
-                // lower_bound(begin, end, val)包含在<algorithm>中
-                // 它的作用是返回有序数组begin..end中第一个大于等于val的元素的迭代器
-                int pos = lower_bound(res.begin(), res.end(), arr[i]) - res.begin();
-                res[pos] = arr[i];
-                maxLen.emplace_back(pos+1);
+                // 注意这里 left <= right 而不是 left < right，我们要替换的是第一个比 arr[i] 大的元素
+                while (left <= right) {
+                    int mid = (right + left) / 2;
+                    if (temp[mid] >= arr[i]) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+                temp[left] = arr[i];
+                nums[i] = left + 1;
             }
         }
-        // 第二步：填充最长递增子序列
-        for (int i = arr.size()-1, j = res.size(); j > 0; --i) {
-            if (maxLen[i] == j) {
-                res[--j] = arr[i];
+
+        int[] res = new int[tempIndex + 1];
+        for (int i = nums.length - 1; i >= 0; --i) {
+            if (nums[i] == tempIndex + 1) {
+                res[tempIndex] = arr[i];
+                --tempIndex;
             }
         }
         return res;
     }
+    /*
+        //todo 自己写的代码2.0版本，但还是没过，很尴尬嘿嘿
+
+        if (arr.length == 1) return arr;
+        int[] lengthArr = new int[arr.length];
+        int[] maxArr = new int[arr.length];
+        //最开始，每个
+        for (int i = 0; i < arr.length; i++) {
+            maxArr[i] = arr[i];
+        }
+        //记录最大长度
+        //计算子序列长度和对应的最大值
+        int maxLength = 0;
+        // todo 这个循环的时间复杂度太高
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    // 记录长度
+                    lengthArr[i] = lengthArr[j] + 1;
+                    // 记录和大小，只记录最小的和
+                    if (maxArr[i] == arr[i] || maxArr[i] > maxArr[j] + arr[i]) {
+                        maxArr[i] = maxArr[j] + arr[i];
+                    }
+                    maxLength = Math.max(maxLength, lengthArr[i]);
+                }
+            }
+        }
+        //获取和最小的子序列索引
+        int index = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = lengthArr.length - 1; i >= 0; i--) {
+            if (lengthArr[i] == maxLength && maxArr[i] < min) {
+                index = i;
+                min = maxArr[i];
+            }
+        }
+        // 填充数据
+        int[] result = new int[maxLength + 1];
+        result[maxLength] = arr[index];
+        int last = index;
+        while (maxLength > 0) {
+            if (arr[index] < arr[last]) {
+                result[--maxLength] = arr[index];
+                last = index;
+            }
+            index--;
+        }
+
+        return result;
      */
 
     /**
@@ -127,9 +175,6 @@ public class NC91 {
      */
     public int maxLength(int[] arr) {
         int[] length = new int[arr.length];
-        for (int i = 0; i < length.length; i++) {
-            length[i] = 1;
-        }
         for (int i = 0; i < arr.length; i++) {
             int val = arr[i];
             for (int j = 0; j < i; j++) {
@@ -142,7 +187,7 @@ public class NC91 {
         for (int i = 0; i < length.length; i++) {
             max = Math.max(length[i], max);
         }
-        return max;
+        return max + 1;
     }
 
 }
